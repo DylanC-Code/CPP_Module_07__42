92864ee5d344ed23dbb7ca5283f30687e0484bc2
dcastor <dcastor@student.42.fr>
2025-08-28 11:15:37 +0200

    feat(00): done

---
+++ ex00/whatever.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 11:10:20 by dcastor           #+#    #+#             */
+/*   Updated: 2025/08/28 11:13:31 by dcastor          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b);
+
+template <typename T>
+T &min(T &a, T &b);
+
+template <typename T>
+T &max(T &a, T &b);
+
+#endif
